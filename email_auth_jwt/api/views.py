7fd90d17458743922e7e785593d775d7f8981c79
JongWook Kim <73515721+mx34kryce@users.noreply.github.com>
2024-01-05 06:48:28 +0000

    몇몇 메시지 수정

--- email_auth_jwt/api/views.py
+++ email_auth_jwt/api/views.py
@@ -22,9 +22,9 @@ def send_email(request):
         auth_email=request.POST['email']
         user=authenticate(email=auth_email,password=auth_pw) #유저 인증
         if(user==None):#유저가 존재하지 않는 경우
-            return HttpResponse("해당 유저가 존재하지 않거나 비밀번호가 일치하지 않습니다.")
+            return JsonResponse({"Status":"Fail","Message":"User does not exist"})
         elif(user.name!=auth_name) or (user.student_id!=auth_student_id):#유저가 존재하지 않는 경우
-            return HttpResponse("입력정보가 유효하지 않습니다")
+            return JsonResponse({"Status":"Fail","Message":"User information is not correct"})
         current_site=get_current_site(request) #현재 사이트 정보 가져오기
         #메시지 발송
         message = render_to_string('mail/activation_email.html', {
@@ -38,7 +38,7 @@ def send_email(request):
         email = EmailMessage(mail_title, message, to=[mail_to])
         email.send()
         #인증 완료 메시지
-        return HttpResponse("인증 메일 발송이 완료되었습니다. 메일을 확인하고 인증을 완료해주세요.")
+        return JsonResponse({"Status":"Success","Message":"Please check your email and complete authentication."})
     else: return render(request,'mail/send.html')
         
 def activate(request, uidb64, token):
@@ -47,18 +47,12 @@ def activate(request, uidb64, token):
         user = User.objects.get(name=uid)
     except(TypeError, ValueError, OverflowError, user.DoesNotExist):
             user = None
-            return HttpResponse("오류가 발생했습니다. 다시 시도해주세요.")
+            return JsonResponse({"Status":"Fail","Message":"Error"})
     if not(user==None) : # 인증하려는 링크가 유효한 링크라면 토큰 발행
         user.is_email_activated = True
         user.save()
-        user_data={
-            "email":f"{user.email}",
-            "password":f"{user.password}",
-        }
-        #print(user_data)
-        response = requests.post(f'http://{get_current_site(request)}/api/token/',json=user_data) # JWT 토큰 발급 // 보안 위험성 확인필요
-        return JsonResponse(response.json(),safe=False)
+        return JsonResponse({"Status":"Success"})
     else:
-        return HttpResponse("유효한 링크가 아닙니다.")
+        return JsonResponse({"Status":"Fail","Message":"Link is not valid"})
     return 
 
--- email_auth_jwt/auth/views.py
+++ email_auth_jwt/auth/views.py
@@ -5,23 +5,26 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 import json
-
+from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 
 @csrf_exempt
 def issue_tokens(request):
     data = json.loads(request.body)
     email = data.get('email')
-    password = data.get('password')
-
-    User = get_user_model()
+    password = str(data.get('password'))
     try:
-        user = User.objects.get(email=email)
-    except User.DoesNotExist:
-        return JsonResponse({'error': 'Invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+        user = get_user_model().objects.get(email=email)
+    except user.DoesNotExist:
+        return JsonResponse({"Status":"Fail","Message":"User does not exist"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    if password!=user.password:
-        return JsonResponse({'error': 'Invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+    if user.check_password(password)==False:
+        return JsonResponse({"Status":"Fail","Message":"Password is not correct"}, 
+                            status=status.HTTP_400_BAD_REQUEST)
+    elif user.is_email_activated==False:
+        return JsonResponse({"Status":"Fail","Message":"User is not activated. Please activate your account first"}, 
+                            status=status.HTTP_400_BAD_REQUEST)
 
     refresh = RefreshToken.for_user(user)
     access_token = str(refresh.access_token)
